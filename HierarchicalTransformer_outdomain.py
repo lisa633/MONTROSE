f79eea354f04937c8107e8573d122bbf73b1283a
root <liushanshan17@nudt.edu.cn>
2025-01-23 03:38:23 +0000

    update 01231136

--- HierarchicalTransformer_outdomain.py
+++ HierarchicalTransformer_outdomain.py
@@ -61,8 +61,8 @@ class SentBert(nn.Module):
         # self.model = BertModel.from_pretrained(bertPath, config=self.bert_config).to(torch.device('cuda'))
         self.model = nn.DataParallel(
             BertModel.from_pretrained(bertPath, config=self.bert_config).to(torch.device('cuda')),
-            # device_ids=[0, 1, 2, 3],
-            device_ids = [0]
+            device_ids=[0, 1, 2, 3],
+            #device_ids = [0]
         )
 
     def text_to_batch_transformer(self, text: List):
@@ -1568,11 +1568,11 @@ class DgMSTF_Trainer(MetaLearningFramework):
 if __name__ == '__main__':
     data_dir1 = r"../../autodl-tmp/data/pheme-rnr-dataset/"
     data_dir2 = r"../../autodl-tmp/data/t1516/"
-    os.environ['CUDA_VISIBLE_DEVICES'] = "0" 
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0,1,2,3" 
 
     events_list = ['charliehebdo', 'ferguson', 'germanwings-crash', 'ottawashooting','sydneysiege','twitter15','twitter16']
     # for domain_ID in range(5):
-    domain_ID = 5
+    domain_ID = 6
     fewShotCnt = 100
     source_events = []
     target_events = []
@@ -1630,7 +1630,7 @@ if __name__ == '__main__':
         else:
             model.save_model(f"../../autodl-tmp/pkl/GpDANN/{test_event_name}/BiGCN_{test_event_name}.pkl")    
 
-    trainer = DgMSTF_Trainer(random_seed=10086, log_dir=logDir, suffix=f"{test_event_name}_FS{fewShotCnt}",model_file=f"../../autodl-tmp/pkl/GpDANN/DgMSTF_{test_event_name}_FS{fewShotCnt}.pkl", domain_num=7,class_num=2, temperature=0.05, learning_rate=5e-5, batch_size=18, epsilon_ball=5e-4,gStep=5, Lambda=0.1, G_lr = 5e-5, D_lr=2e-3, valid_every=10, dStep=20) 
+    trainer = DgMSTF_Trainer(random_seed=10086, log_dir=logDir, suffix=f"{test_event_name}_FS{fewShotCnt}",model_file=f"../../autodl-tmp/pkl/GpDANN/DgMSTF_{test_event_name}_FS{fewShotCnt}.pkl", domain_num=7,class_num=2, temperature=0.05, learning_rate=5e-5, batch_size=32, epsilon_ball=5e-4,gStep=5, Lambda=0.1, G_lr = 5e-5, D_lr=2e-3, valid_every=10, dStep=20) 
     bert_config = BertConfig.from_pretrained(bertPath,num_labels = 2)
     model_device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     discriminator = DomainDiscriminator(hidden_size=bert_config.hidden_size,
